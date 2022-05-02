ee52b5e68ec64d8dae8aed1906edad1eb162d98e
Илья Зотов <ilazotov@MacBook-Air-Ila.local>
2022-05-02 12:17:48 +0300

    round

--- src/components/counter/counter.js
+++ src/components/counter/counter.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import classes from "../../App.css"
 function Counter(props){
     return(<div>
-       <p className={classes.textcounter}>pressing: {props.counter}</p>
+       <p className={classes.textcounter}>pressed: {props.counter}</p>
     </div>)
 }
 function mapStateToProps(state){  
